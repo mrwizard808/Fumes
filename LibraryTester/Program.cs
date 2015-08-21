12cba5deb34b7dfa14fc9fca90b05f8c9e51ad5b
OppositeVector <iloriginal@gmil.com>
2015-08-21 20:46:20 +0300

    First commit

---
+++ LibraryTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fumes;
+
+namespace LibraryTester {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            FumesController controller = new FumesController();
+            NetworkOperation op = new NetworkOperation("", (string data) => {
+                Console.WriteLine(data);
+            });
+            controller.AddOperation(op);
+
+        }
+
+    }
+
+}
---
+++ SteamInformer/NetworkOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fumes {
+
+    public class NetworkOperation : INetworkOperation {
+
+        public string urlParams { get; private set; }
+        public Action<string> callback { get; private set; }
+
+        public NetworkOperation(string url, Action<string> callback) {
+            this.urlParams = url;
+            this.callback = callback;
+        }
+
+    }
+
+}
