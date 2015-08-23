5d8444362c565b1ae9b72c718e71a93d30034406
mrwizard808 <alon_nevo@live.com>
2015-08-24 00:45:23 +0300

    dropped the base

--- SteamInformer/UserLogIn.cs
+++ SteamInformer/UserLogIn.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace Fumes
 {
-    class UserLogIn
+    class UserLogIn  
     {
+        string LogInURL = "http://api.steampowerd.com/";
+        public UserLogIn(Action<string> callback)
+        {
+            WebRequest req = WebRequest.Create(LogInURL);
+            req.Method = "POST";
+
+            //
+        }
     }
 }
